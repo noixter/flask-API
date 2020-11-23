7b119169e1de1394d30932686b3412b305f48c39
Sebastian <jsebas791@gmail.com>
2020-11-22 20:03:22 -0500

    added a celery app

--- app/datastore/__init__.py
+++ app/datastore/__init__.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient
+from celery import Celery
+import os
 
 datastore = Blueprint('data', __name__, url_prefix='/data')
 db = SQLAlchemy()
 client = MongoClient('localhost', 27017)
-
+broker = Celery('publisher', broker='amqp://{}:{}@{}/'.format(os.environ.get('USER_BROKER'),
+                                                              os.environ.get('PASSWD_BROKER'),
+                                                              os.environ.get('URL_BROKER')))
 
 from . import views
