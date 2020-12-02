3afa06e988107f031b5cb3058916cd4c52933703
Sebastian Rocha <jsebas791@gmail.com>
2020-12-02 16:52:41 -0500

    Removed load_dotenv methods, change database connection

--- app/Config.py
+++ app/Config.py
@@ -1,10 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 class Config:
     SECRET_KEY = 'mysecretkey'
     DEBUG = True
     ENV = 'development'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost:5432/users'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'),
+                                                                   os.getenv('DB_URL'), str(os.getenv('DB_PORT')),
+                                                                   os.getenv('DB_NAME'))
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
