c339cb9315718caad169b7a677ecce0629f6c358
Sebastian Rocha <jsebas791@gmail.com>
2020-12-02 16:46:29 -0500

    Updated broker connection with env variables

--- app/datastore/__init__.py
+++ app/datastore/__init__.py
@@ -1,14 +1,17 @@
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient
-from celery import Celery
+from kombu import Connection
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 datastore = Blueprint('data', __name__, url_prefix='/data')
 db = SQLAlchemy()
-client = MongoClient('localhost', 27017)
-broker = Celery('publisher', broker='amqp://{}:{}@{}/'.format(os.environ.get('USER_BROKER'),
-                                                              os.environ.get('PASSWD_BROKER'),
-                                                              os.environ.get('URL_BROKER')))
+#client = MongoClient('localhost', 27017)
+broker = Connection('pyamqp://{}:{}@{}:{}//'.format(str(os.getenv('BROKER_USER')), str(os.getenv('BROKER_PASS')),
+                                                    str(os.getenv('BROKER_URL')), str(os.getenv('BROKER_PORT'))))
+
 
 from . import views
