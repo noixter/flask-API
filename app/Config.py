7b7cb6cc404cdb4423d3062dd6c763473d662969
Sebastian <jsebas791@gmail.com>
2020-12-05 13:27:44 -0500

    added setup to psql by heroku

--- app/Config.py
+++ app/Config.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 load_dotenv()
 
@@ -9,8 +10,9 @@ class Config:
     SECRET_KEY = 'mysecretkey'
     DEBUG = True
     ENV = 'development'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'),
-                                                                   os.getenv('DB_URL'), str(os.getenv('DB_PORT')),
-                                                                   os.getenv('DB_NAME'))
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'),
+    #                                                               os.getenv('DB_URL'), str(os.getenv('DB_PORT')),
+    #                                                               os.getenv('DB_NAME'))
+    SQLALCHEMY_DATABASE_URI = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
