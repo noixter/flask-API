8c413d13d4545308b454327ec6daaf42af30f4b8
Sebastian <jsebas791@gmail.com>
2020-12-06 16:22:44 -0500

    DB Connection modified

--- app/Config.py
+++ app/Config.py
@@ -13,6 +13,6 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'),
     #                                                               os.getenv('DB_URL'), str(os.getenv('DB_PORT')),
     #                                                               os.getenv('DB_NAME'))
-    SQLALCHEMY_DATABASE_URI = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
