2aec8b34b04139481315ee20eec25e69fa5de30d
Sebastian Rocha <jsebas791@gmail.com>
2021-01-21 19:05:31 -0500

    feat: Modified model behave with serializing method

--- app/users/models.py
+++ app/users/models.py
@@ -1,22 +1,19 @@
 from . import db
-from flask import request
 from sqlalchemy.exc import IntegrityError
-from flask_login import UserMixin, current_user
-from flask_serialize import FlaskSerializeMixin
-from marshmallow_sqlalchemy import ModelSchema
-
-FlaskSerializeMixin.db = db
+from sqlalchemy.orm.exc import FlushError
+from flask_login import UserMixin
 
 
-class Role(FlaskSerializeMixin, db.Model):
+class Role(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String, nullable=False)
 
     def __str__(self):
         return '[{}] {}'.format(self.id, self.name)
 
 
-class Users(UserMixin, FlaskSerializeMixin, db.Model):
+class Users(UserMixin, db.Model):
     """Class Users, manage the general users
         extends UserMixin for implements flask-login
         add attributes: is_active, is_authenticated for manage session
@@ -31,75 +28,30 @@ class Users(UserMixin, FlaskSerializeMixin, db.Model):
     rol_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
     rol = db.relationship('Role', backref=db.backref('Users', lazy=True))
 
-    exclude_serialize_fields = ['is_anonymous', 'is_authenticated', 'is_active', 'password']
-    create_fields = ['id', 'user_id', 'first_name', 'last_name', 'email', 'password', 'position', 'rol_id']
-    update_fields = ['user_id', 'first_name', 'last_name', 'email', 'password', 'position', 'rol_id']
-
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
     def get_id(self):
         return self.user_id
 
-    def can_access(self):
-        """Authentication Method
-            :returns: True if is admin or if method are GET, POST, PUT only if current user
-            is equal to item to update
-            :returns: False if method is DELETE and is the same user
-            and every case else
-        """
-        if current_user.rol_id == 1:
-            return True
-        elif request.method == 'GET':
-            return True
-        elif request.method == ('POST' or 'PUT') and current_user == self:
-            return True
-        elif request.method == 'DELETE' and current_user.user_id == self.user_id:
-            return False
-        else:
-            return False
-
-    def can_delete(self):
-        """Only admins can DELETE
-            :throws: Exception if its other kind of user
-        """
-        if current_user.rol_id == 1:
-            return True
-        elif current_user == self:
-            raise Exception('Not Allowed delete yourself')
-        else:
-            raise Exception('Delete Not Allowed')
-
     def create_object(self):
-        if current_user.rol_id == 1:
-            try:
-                db.session.add(self)
-            except IntegrityError:
-                raise Exception('User {} already exists'.format(self))
-            db.session.commit()
-        else:
-            raise Exception('No admin users can not delete')
+        try:
+            db.session.add(self)
+        except (IntegrityError, FlushError):
+            raise Exception('User {} already exists'.format(self))
+        db.session.commit()
 
     def update_object(self, updated_fields):
-        if current_user.rol_id == 1 or current_user == self:
-            try:
-                self.update_from_dict(updated_fields)
-            except IntegrityError:
-                raise Exception('User does not exist')
-            db.session.commit()
-        else:
-            raise Exception('Not allowed to change this user')
-
+        for field in updated_fields:
+            setattr(self, field, updated_fields[field])
+        db.session.commit()
 
     def delete_user(self):
-        if current_user.rol_id == 1 or current_user != self:
-            try:
-                db.session.delete(self)
-            except IntegrityError:
-                raise Exception('User {} can not be deleted'.format(self))
-            db.session.commit()
-        else:
-            raise Exception('No admin users can not delete')
+        try:
+            db.session.delete(self)
+        except IntegrityError:
+            raise Exception('User {} can not be deleted'.format(self))
+        db.session.commit()
 
 
 
